820be5a2e6f33e5c658490095cf3f73bd96e3050
AKHRIFJaafar <Akhrif.jaafar.dev@gmail.com>
2021-04-22 13:27:16 +0000

    Revert "Save Changes"
    This reverts commit 1223edc9d308eb9301b32c22e8df470c4566e652.

--- Prototype - Code Source/index.php
+++ Prototype - Code Source/index.php
@@ -1,32 +1,21 @@
-<?php
-
-// Class Creation
-class Calculatrice{
-    private $x ;
-    private $y ;
-    private $operation ;
-
-    function __construct($x,$y,$operation) {
-        $this->x = $x;
-        $this->y = $y;
-        $this->operation = $operation;
-      }
-      function Calculer(){
-          $solution = null ;
-          switch ($this->operation) {
-            case '+':
-                $solution = $this->x + $this->y ;
-                break;
-            case '-':
-                $solution = $this->x - $this->y ;
-                break;
-              default:
-                  break;
-          }
-          return $solution ; 
-      }
+<?php 
+function Calculer($x, $y, $operation){
+    $solution = null ;
+    switch ($operation) {
+        case '+':
+            $solution = $x + $y ;
+            break;
+        case '+':
+            $solution = $x - $y ;
+            break;
+        case '/':
+            $solution = $x / $y ;
+            break;
+        default:
+            break;
+    }
+    return $solution; 
 }
-
 //Initialisation
 $x = null ;
 $y = null ;
@@ -34,41 +23,46 @@ $operation = null ;
 $solution = null ;
 $afficheur = "" ;
 
+ // Traitement
+ if(isset($_POST['x'])) $x = $_POST['x'];
+ if(isset($_POST['y'])) $y = $_POST['y'];
+ if(isset($_POST['operation'])) $operation = $_POST['operation'];
 
-if(isset($_POST['x'])) $x = $_POST['x'];
-if(isset($_POST['y'])) $x = $_POST['y'];
-if(isset($_POST['operation'])) $x = $_POST['operation'];
-
-
-//Ajouter La Valeur
-
-if (isset($_POST['Number'])) {
+// Ajouter la valeur du nombre au X ou Y
+if(isset($_POST['Number'])){
     $Number = $_POST['Number'] ;
-    if($operation == null){
-        if($x == null) $x = $Number ;
-        else $x = floatval($x . $Number);
+    if ($operation == null) {
+        if ($x == null) $x = $_POST['Number'];
+        else $x = floatval($x . $Number) ;
     }else {
-        if($y == null) $y = $Number ;
-        else $y = floatval($y . $Number);
-    } 
+        if ($y == null) $y = $_POST['Number'];
+        else $y = floatval($y . $Number) ;
+    }
 }
-//Calcule
-if(isset($_POST['Egale'])){
+
+if (isset($_POST['Egale'])) {
     $Egale = $_POST['Egale'] ;
-    $Calculatrice = new Calculatrice($x, $y, $operation);
-    $solution = $Calculatrice->Calculer($x, $y, $operation) ;
+    $solution = Calculer($x,$y,$operation) ;
 }
 
-//Afficher
-if($solution != null)
-    $afficheur = $solution ;
+//Affichage 
+if ($solution != null) $afficheur = $solution ;
 else {
-    if($x != null ) $afficheur .= $x ;
-    if($operation != null ) $afficheur .= "" . $operation ."" ;
-    if($y != null ) $afficheur .= $y ;
+    if ($x != null) $afficheur = $afficheur . $x ;
+    if ($operation != null) $afficheur .= "" . $operation . "";
+    if ($y != null) $afficheur .= $y ;
 }
-?>
 
+// C Button 
+if(isset($_POST['init'])){
+    $x = null ;
+    $y = null ;
+    $operation = null ;
+    $solution = null ;
+    $afficheur = "" ;
+}
+
+?>
 
 <html lang="en">
 <head>
@@ -79,21 +73,34 @@ else {
 </head>
 <body>
 <form action="" method="POST">
-<input type="text" name="x" value="<?php echo $x ?> 1">
-<input type="text" name="y" value="<?php echo $y ?>2">
-<input type="text" name="operation" value="<?php echo $operation ?>">
+<input type="hidden" name="x" value="<?php echo $x ?>"  ></input>
+<input type="hidden" name="y" value="<?php echo $y ?>"  ></input>
+<input type="hidden" name="operation" value="<?php echo $operation ?>" ></input>
+<br><br>
+<input type="text" name="afficheur" value="<?php echo $afficheur ?>"  ></input>
+<br><br>
+<input type="submit" name="Number" value="1"></input>
+<input type="submit" name="Number" value="2"></input>
+<input type="submit" name="Number" value="3"></input>
+<input type="submit" name="init" value="C" ></input>
 <br><br>
 
-<input type="text" name="afficheur" id="">
+<input type="submit" name="Number" value="4"></input>
+<input type="submit" name="Number" value="5"></input>
+<input type="submit" name="Number" value="6"></input>
+<input type="submit" name="operation" value="+"></input>
 <br><br>
 
-<input type="submit" name="Number" value="1" id="">
-<input type="submit" name="Number" value="2" id="">
-<input type="submit" name="operation" value="+" id="">
-<input type="submit" name="operation" value="-" id="">
+<input type="submit" name="Number" value="7"></input>
+<input type="submit" name="Number" value="8"></input>
+<input type="submit" name="Number" value="9"></input>
+<input type="submit" name="operation" value="-"></input>
 <br><br>
 
-<input type="submit" name="Egale" value="Calculer" id="">
+<input type="submit" name="Egale" value="Calculer" ></input>
+<input type="submit" name="operation" value="/"></input>
+
 </form>
+    
 </body>
 </html>
