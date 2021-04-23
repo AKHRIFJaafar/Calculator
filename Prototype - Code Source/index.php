1e4dc4a11472123d87c3637de8967aa8d0275d03
AKHRIFJaafar <Akhrif.jaafar.dev@gmail.com>
2021-04-23 11:34:01 +0000

    Prototype-Class Done

--- Prototype - Code Source/index.php
+++ Prototype - Code Source/index.php
@@ -1,76 +1,80 @@
 <?php
 
-// Class Creation
 class Calculatrice{
-    private $x ;
-    private $y ;
-    private $operation ;
+    private $x;
+    private $y;
+    private $operation;
 
-    function __construct($x,$y,$operation) {
-        $this->x = $x;
-        $this->y = $y;
+    function __construct($a,$b,$operation) {
+        $this->x = $a;
+        $this->y = $b;
         $this->operation = $operation;
-      }
-      function Calculer(){
-          $solution = null ;
-          switch ($this->operation) {
-            case '+':
-                $solution = $this->x + $this->y ;
+    }
+ 
+    function Calculer(){
+        $solution = null;
+        switch($this->operation){
+            case "+" : $solution = $this->x + $this->y;
                 break;
-            case '-':
-                $solution = $this->x - $this->y ;
+            case "-" : $solution = $this->x - $this->y;
                 break;
-              default:
-                  break;
-          }
-          return $solution ; 
-      }
-}
-
-//Initialisation
-$x = null ;
-$y = null ;
-$operation = null ;
-$solution = null ;
-$afficheur = "" ;
-
-
-if(isset($_POST['x'])) $x = $_POST['x'];
-if(isset($_POST['y'])) $x = $_POST['y'];
-if(isset($_POST['operation'])) $x = $_POST['operation'];
-
-
-//Ajouter La Valeur
-
-if (isset($_POST['Number'])) {
-    $Number = $_POST['Number'] ;
-    if($operation == null){
-        if($x == null) $x = $Number ;
-        else $x = floatval($x . $Number);
-    }else {
-        if($y == null) $y = $Number ;
-        else $y = floatval($y . $Number);
+        }
+        return $solution;
     } 
-}
-//Calcule
-if(isset($_POST['Egale'])){
-    $Egale = $_POST['Egale'] ;
-    $Calculatrice = new Calculatrice($x, $y, $operation);
-    $solution = $Calculatrice->Calculer($x, $y, $operation) ;
-}
 
-//Afficher
-if($solution != null)
-    $afficheur = $solution ;
-else {
-    if($x != null ) $afficheur .= $x ;
-    if($operation != null ) $afficheur .= "" . $operation ."" ;
-    if($y != null ) $afficheur .= $y ;
 }
+   
+
+    // Initialisation des variables
+    $x = null;
+    $y = null;
+    $operation = null;
+    $afficheur = "";
+    $solution = null;
+
+    // Traitement
+
+    // Récupération des variables de la page
+    if(isset($_POST['x'])) $x = $_POST['x'];
+    if(isset($_POST['y']))$y = $_POST['y'];
+    if(isset($_POST['operation'])) $operation = $_POST['operation'];
+
+    // Ajouter la valeur du Number au X ou Y
+    if(isset($_POST['Number'])){
+        $Number = $_POST['Number'];
+        if($operation == null){
+            if($x == null) $x = $Number;
+            else $x = floatval($x . $Number);
+        }else{
+            if($y == null) $y = $Number;
+            else $y = floatval($y . $Number);
+        }
+    }
+
+    if(isset($_POST['Egale'])){
+        $Egale = $_POST['Egale'];
+        $calculatrice = new Calculatrice($x,$y,$operation);
+        $solution = $calculatrice->Calculer($x,$y,$operation);
+      
+    }
+    // Affichage 
+    if($solution != null) $afficheur = $solution;
+    else{
+        if($x != null) $afficheur = $afficheur . "$x" ;
+        if($operation != null) $afficheur .= " " .  $operation . " ";
+        if($y != null) $afficheur .= $y;
+    }
+if(isset($_POST['Init'])){
+    $x = null;
+    $y = null;
+    $operation = null;
+    $afficheur = "";
+    $solution = null;
+}
+     
 ?>
-
-
-<html lang="en">
+<!DOCTYPE html>
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -78,22 +82,23 @@ else {
     <title>Calculatrice</title>
 </head>
 <body>
-<form action="" method="POST">
-<input type="text" name="x" value="<?php echo $x ?> 1">
-<input type="text" name="y" value="<?php echo $y ?>2">
-<input type="text" name="operation" value="<?php echo $operation ?>">
-<br><br>
-
-<input type="text" name="afficheur" id="">
-<br><br>
-
-<input type="submit" name="Number" value="1" id="">
-<input type="submit" name="Number" value="2" id="">
-<input type="submit" name="operation" value="+" id="">
-<input type="submit" name="operation" value="-" id="">
-<br><br>
-
-<input type="submit" name="Egale" value="Calculer" id="">
+<form action="" method="post">
+    <input type="hidden" name="x" value="<?php echo $x ?>">
+    <input type="hidden" name="y" value="<?php echo $y ?>">
+    <input type="hidden" name="operation" value="<?php echo $operation ?>">
+    <br>
+    <input type="text" id="afficheur" name="afficheur" value="<?php echo $afficheur ?>" />
+    <br><br>
+    <input type="submit" name="Number" value="1"  ></input>
+    <input type="submit" name="Number" value="2"  ></input>
+    <input type="submit" name="Number" value="3"  ></input>
+    <input type="submit" name="Init" value="C"  ></input>
+    <br><br>
+    <input type="submit" name="operation" value="+"  ></input>
+    <input type="submit" name="operation" value="-"  ></input>
+    <br><br>
+    <input type="submit" name="Egale" value="Calculer"  ></input>
 </form>
+    
 </body>
 </html>
